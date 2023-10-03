873b006593b5a667d9b221ff588cc0579d81f79b
Alexis-NM <alexis.na.me@gmail.com>
2023-10-03 11:06:10 +0200

    wilders

---
+++ index.js
@@ -0,0 +1,4 @@
+import chalk from "chalk";
+
+const log = console.log;
+log("Zomb Nwar Members are " + chalk.blue('Calyspo ') + chalk.red('Maxence ') + chalk.green('Jean-Louis ') + chalk.yellow('Alexis'));
